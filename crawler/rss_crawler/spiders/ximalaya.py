df7a6a975355da9b46b23fc6d1c03d13a7ecab40
anribras <bravo911@163.com>
2021-11-07 18:01:09 +0800

    updated

--- crawler/rss_crawler/spiders/ximalaya.py
+++ crawler/rss_crawler/spiders/ximalaya.py
@@ -6,6 +6,7 @@ import re
 
 CATE_INFO = 'https://www.ximalaya.com/revision/category/allCategoryInfo'
 CATE_INFO_STORE = 'ximalaya_categories.json'
+ALBUM_INFO = 'https://www.ximalaya.com/revision/album/v1/simple?albumId='
 
 
 def parse_cate_from_file(file):
@@ -18,15 +19,16 @@ def parse_cate_from_file(file):
 
 class XimalayaSpider(scrapy.Spider):
     name = 'ximalaya'
+    url = ''
     json_obj = parse_cate_from_file(CATE_INFO_STORE)
     if json_obj is not None:
-        cate_lv1 = set(json_obj['cates_lv1'])
-        cate_lv2 = set(json_obj['cates_lv2'])
-        cate_lv3 = set(json_obj['cates_lv3'])
+        cate_lv1 = json_obj['cates_lv1']
+        cate_lv2 = json_obj['cates_lv2']
+        cate_lv3 = json_obj['cates_lv3']
     else:
-        cate_lv1 = set()
-        cate_lv2 = set()
-        cate_lv3 = set()
+        cate_lv1 = {}
+        cate_lv2 = {}
+        cate_lv3 = {}
     my_category = {
         'channel': [],
         'category_lv1': [],
@@ -34,7 +36,8 @@ class XimalayaSpider(scrapy.Spider):
         'category_lv3': [],
     }
     my_author = {}
-    my_album = {}
+    my_album = {'xima':{}}
+    my_tracks = {'xima':{}}
 
     def __has_cates_info(self):
         return len(self.cate_lv1) and len(self.cate_lv2) and len(self.cate_lv3)
@@ -42,19 +45,32 @@ class XimalayaSpider(scrapy.Spider):
     def start_requests(self):
         if not self.__has_cates_info():
             yield scrapy.Request(CATE_INFO, self.pare_cate_info)
-        url = getattr(self, 'url', None)
+
+        self.url = url = getattr(self, 'url', None)
         yield scrapy.Request(url, self.parse)
 
+        self.my_album['xima']['id'] = album_id = url.split('/')[-2]
+        yield scrapy.Request(ALBUM_INFO + album_id, self.parse_album)
+
+
     def parse(self, response, **kwargs):
+
         self.parse_cate(response, **kwargs)
         self.parse_author(response, **kwargs)
-        yield {
-            'category_lv1': list(self.my_category['category_lv1']),
-            'category_lv2': list(self.my_category['category_lv2']),
-            'category_lv3': list(self.my_category['category_lv3']),
-            'channel': self.my_category['channel'],
-            'author': self.my_author
-        }
+
+        track_page_nums = response.css('ul.pagination-page li:nth-last-child(2) span::text').get()
+        track_urls = [self.url+'p'+ str(index) for index in range(1,int(track_page_nums)+1)]
+
+        # self.parse_tracks(response, track_urls=track_urls)
+
+        # yield {
+        #     'category_lv1': self.my_category['category_lv1'],
+        #     'category_lv2': self.my_category['category_lv2'],
+        #     'category_lv3': self.my_category['category_lv3'],
+        #     'channel': self.my_category['channel'],
+        #     'author': self.my_author,
+        #     'album': self.my_album
+        # }
 
     def pare_cate_info(self, response):
         """
@@ -63,18 +79,24 @@ class XimalayaSpider(scrapy.Spider):
         # for json dot operation
         info = response.json()
         for lv1 in info['data']:
-            self.cate_lv1.add(lv1['name'])
+            self.cate_lv1[lv1['name']] = {}
             for lv2 in lv1['categories']:
-                self.cate_lv2.add(lv2['displayName'])
+                self.cate_lv2[lv2['displayName']] = {
+                    "pinyin": lv2['pinyin'],
+                    "link": lv2['link']
+                }
                 for lv3 in lv2['subcategories']:
-                    self.cate_lv3.add(lv3['displayValue'])
-        with open(CATE_INFO_STORE, 'w',encoding='utf-8') as f:
+                    self.cate_lv3[lv3['displayValue']] = {
+                        "pinyin": lv3['code'],
+                        "link": lv3['link'],
+                    }
+        with open(CATE_INFO_STORE, 'w', encoding='utf-8') as f:
             f.write(
                 json.dumps(
                     {
-                        'cates_lv1': list(self.cate_lv1),
-                        'cates_lv2': list(self.cate_lv2),
-                        'cates_lv3': list(self.cate_lv3)
+                        'cates_lv1': self.cate_lv1,
+                        'cates_lv2': self.cate_lv2,
+                        'cates_lv3': self.cate_lv3
                     },
                     ensure_ascii=False
                 )
@@ -83,19 +105,37 @@ class XimalayaSpider(scrapy.Spider):
     def parse_cate(self, response, **kwargs):
         tag_titles = response.css('span.xui-tag-text a::attr(title)').getall()
         tag_hrefs = response.css('span.xui-tag-text a::attr(href)').getall()
+        """
+        Speed up
+        """
         for index, href in enumerate(tag_hrefs):
             tag = tag_titles[index]
             if not re.match('/channel/', href):
-                if tag in self.cate_lv1:
-                    self.my_category['category_lv1'].append(tag)
-                elif tag in self.cate_lv2:
-                    self.my_category['category_lv2'].append(tag)
-                elif tag in self.cate_lv3:
-                    self.my_category['category_lv3'].append(tag)
+                if tag in self.cate_lv1.keys():
+                    self.my_category['category_lv1'].append({
+                        **self.cate_lv1[tag],
+                        "name": tag
+                    })
+                elif tag in self.cate_lv2.keys():
+                    self.my_category['category_lv2'].append({
+                        **self.cate_lv2[tag],
+                        "name": tag
+                    })
+                elif tag in self.cate_lv3.keys():
+                    self.my_category['category_lv3'].append({
+                        **self.cate_lv3[tag],
+                        "name": tag
+                    })
                 else:
                     logging.error('Tag parse error!')
             else:
                 self.my_category['channel'].append(tag)
+        yield {
+            'category_lv1': self.my_category['category_lv1'],
+            'category_lv2': self.my_category['category_lv2'],
+            'category_lv3': self.my_category['category_lv3'],
+            'channel': self.my_category['channel'],
+        }
 
     def parse_author(self, response):
         try:
@@ -105,5 +145,17 @@ class XimalayaSpider(scrapy.Spider):
             self.my_author['url'] = url
             self.my_author['nickname'] = response.css('p.anchor-info-nick a::text').get()
             self.my_author['introduction'] = response.css('p.anchor-intro::text').getall()[1]
+            yield {
+                'author': self.my_author,
+            }
         except ValueError:
             logging.error('Avatar error')
+
+    def parse_album(self, response):
+        self.my_album['xima']['info'] = response.json()
+        yield {
+            'album': self.my_album
+         }
+
+    def parse_tracks(self, response, **kwargs):
+        pass
